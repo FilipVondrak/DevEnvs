d93fb6a75e6331aad639a59427dccd6ea8bd420b
Filip Vondrák <90453814+FilipVondrak@users.noreply.github.com>
2026-08-21 14:08:12 +0200

    update dotnet flake

--- dotnet/flake.nix
+++ dotnet/flake.nix
@@ -2,10 +2,13 @@
   description = "C# development environment";
 
   inputs = {
+    # currently latest packages
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # reference to older specific commit containing dotnet 10.0.101 due to IDE compatibility
+    nixpkgs-dotnet10.url = "github:NixOS/nixpkgs/f665af0cdb70ed27e1bd8f9fdfecaf451260fc55";
   };
 
-  outputs = { self, nixpkgs, ... } @ inputs:
+  outputs = { self, nixpkgs, nixpkgs-dotnet10, ... } @ inputs:
     let
       supportedSystems = [
         "aarch64-darwin"
@@ -17,9 +20,10 @@
     in {
       devShells = forAllSystems (system: let
         pkgs = import nixpkgs { inherit system; };
+        pkgs10 = import nixpkgs-dotnet10 { inherit system; };
 
         combined-dotnet = pkgs.dotnetCorePackages.combinePackages [
-          pkgs.dotnet-sdk_10
+          pkgs10.dotnet-sdk_10
           pkgs.dotnet-sdk_9
         ];
 
